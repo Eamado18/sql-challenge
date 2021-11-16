d728ce3a501418a6a7b1deba01077fcfbd507a82
Eamado18 <89824082+Eamado18@users.noreply.github.com>
2021-11-15 18:21:17 -0800

    Update Query_4.sql

--- EmployeeSQL/Query_4.sql
+++ EmployeeSQL/Query_4.sql
@@ -1,1 +1,7 @@
-
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+select employees.emp_no, Last_name, First_name, departments.dept_name
+from employees
+join dept_emp
+on employees.emp_no = dept_emp.emp_no
+join departments
+on departments.dept_no = dept_emp.dept_no
