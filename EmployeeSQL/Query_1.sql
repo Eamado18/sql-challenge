7daf56e07ab17913728b17c0856152db739827fd
Eamado18 <89824082+Eamado18@users.noreply.github.com>
2021-11-15 18:20:14 -0800

    Update Query_1.sql

--- EmployeeSQL/Query_1.sql
+++ EmployeeSQL/Query_1.sql
@@ -1,1 +1,6 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+select employees.emp_no, salaries.salary, Last_name, First_name, Sex
+from employees
+join salaries 
+on employees.emp_no = salaries.emp_no; 
 
