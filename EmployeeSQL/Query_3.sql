72b96da477fe0ac244616f9e7f39d08a97010508
Eamado18 <89824082+Eamado18@users.noreply.github.com>
2021-11-15 18:20:55 -0800

    Update Query_3.sql

--- EmployeeSQL/Query_3.sql
+++ EmployeeSQL/Query_3.sql
@@ -1,1 +1,7 @@
-
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+select dept_manager.dept_no, dept_manager.emp_no, departments.dept_name, employees.last_name, employees.first_name
+from dept_manager 
+join departments 
+on dept_manager.dept_no = departments.dept_no 
+join employees 
+on dept_manager.emp_no = employees.emp_no 
