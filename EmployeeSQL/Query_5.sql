835874acf92502453bf123e4d71ab0d0ca1e5138
Eamado18 <89824082+Eamado18@users.noreply.github.com>
2021-11-15 18:21:36 -0800

    Update Query_5.sql

--- EmployeeSQL/Query_5.sql
+++ EmployeeSQL/Query_5.sql
@@ -1,1 +1,4 @@
-
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select First_name, Last_name, Sex
+from employees
+where First_name = 'Hercules' AND Last_name like 'B%';
