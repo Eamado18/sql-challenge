a2ab9a671c84a42d97ad1f4d1b196aa0c4d5c75a
Eamado18 <89824082+Eamado18@users.noreply.github.com>
2021-11-15 18:20:37 -0800

    Update Query_2.sql

--- EmployeeSQL/Query_2.sql
+++ EmployeeSQL/Query_2.sql
@@ -1,1 +1,4 @@
-
+--List first name, last name, and hire date for employees who were hired in 1986.
+select First_name, Last_name, Hire_date
+from employees
+where DATE_PART('year',hire_date) = 1986
