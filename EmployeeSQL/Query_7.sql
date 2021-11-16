23c6b8b06e218587aa11ec13a1163c9f0bdf6455
Eamado18 <89824082+Eamado18@users.noreply.github.com>
2021-11-15 18:22:22 -0800

    Update Query_7.sql

--- EmployeeSQL/Query_7.sql
+++ EmployeeSQL/Query_7.sql
@@ -1,1 +1,8 @@
-
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select employees.emp_no, Last_name, First_name, departments.dept_name
+from employees 
+join dept_emp 
+on employees.emp_no = dept_emp.emp_no
+join departments
+on departments.dept_no = dept_emp.dept_no
+where departments.dept_name in ('Sales')
