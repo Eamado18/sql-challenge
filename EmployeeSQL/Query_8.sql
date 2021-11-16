4fae453e8cfd2410ea621423e8a19a8dee94db08
Eamado18 <89824082+Eamado18@users.noreply.github.com>
2021-11-15 18:22:38 -0800

    Update Query_8.sql

--- EmployeeSQL/Query_8.sql
+++ EmployeeSQL/Query_8.sql
@@ -1,1 +1,5 @@
-
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(*) 
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(*)  DESC;
